67e2983829a2426349644b8227e777b0c69f9161
Taras Nesterenko <tnestere@gmail.com>
2023-12-16 19:04:00 +0100

    Updates

---
+++ precache-manifest.de81b9b5cc243aacc25897ff57d63163.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7a9ce0a7564dab9a403",
+    "url": "/social-network-samurai/static/css/main.ce119a45.chunk.css"
+  },
+  {
+    "revision": "c7a9ce0a7564dab9a403",
+    "url": "/social-network-samurai/static/js/main.c7a9ce0a.chunk.js"
+  },
+  {
+    "revision": "6ce80e5d238c1dbcddd2",
+    "url": "/social-network-samurai/static/js/1.6ce80e5d.chunk.js"
+  },
+  {
+    "revision": "f28ab7c9916095e7747d",
+    "url": "/social-network-samurai/static/js/runtime~main.f28ab7c9.js"
+  },
+  {
+    "revision": "34c3332cb8eb6c448bb4544cd7df4bcd",
+    "url": "/social-network-samurai/static/media/user.34c3332c.png"
+  },
+  {
+    "revision": "d1f91937eb7b09d650be161b807d2938",
+    "url": "/social-network-samurai/static/media/preloader.d1f91937.svg"
+  },
+  {
+    "revision": "f5d8a9bbcd7fc9fc16820c8040cfa7f9",
+    "url": "/social-network-samurai/index.html"
+  }
+];
